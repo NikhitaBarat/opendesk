4213ca2eb22a8e3446ec415aca78f30d78f866fa
Siddhant Prateek <siddhantprateek@gmail.com>
2023-02-18 21:47:02 +0530

    API handlers and controllers

---
+++ handlers/handlers.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"encoding/json"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	connection "github.com/nikhitabarat/opendesk/connection"
+	controllers "github.com/nikhitabarat/opendesk/handlers/controllers"
+)
+
+// @desp: Opendesk API Handlers
+func OpenDeskApiHandler() {
+	router := fiber.New(fiber.Config{
+		JSONEncoder: json.Marshal,
+		JSONDecoder: json.Unmarshal,
+	})
+
+	router.Use(cors.New())
+
+	// Connect Database
+	connection.MongoDBdatabase()
+
+	router.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Server is Healthy")
+	})
+
+	router.Post("/task", controllers.AddTask)
+	router.Get("/task/:taskId", controllers.GetTaskbyId)
+	router.Get("/tasks", controllers.GetEmployeeTask)
+	router.Delete("/task/:taskId", controllers.DeleteTaskbyId)
+
+	router.Get("/api", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
+	})
+	router.Listen(":8090")
+
+}

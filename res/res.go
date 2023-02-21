37e59950dc442352083401850d64aa425ab5a7f3
Siddhant Prateek <siddhantprateek@gmail.com>
2023-02-21 15:42:39 +0530

    response type renamed and fly deployment

--- res/res.go
+++ res/res.go
@@ -2,7 +2,7 @@ package res
 
 import "github.com/gofiber/fiber/v2"
 
-type TaskResponse struct {
+type ResponseType struct {
 	Status  int        `json:"status"`
 	Message string     `json:"message"`
 	Data    *fiber.Map `json:"data"`
